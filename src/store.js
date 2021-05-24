4fe6637d8a9ac23c9226939cbc47a49ebd2eeed1
fengyb <fengyb@dazhuanjia.com>
2021-05-24 15:03:16 +0800

    feat: 使用原生react-redux

---
+++ src/store.js
@@ -0,0 +1,14 @@
+import { createStore } from "redux";
+
+
+const countReducer = (state = 0, action) => {
+    switch (action.type) {
+        case 'ADD': return state + 1;
+        case 'MINUS': return state - 1;
+        default: return state
+    }
+}
+
+const store = createStore(countReducer)
+
+export default store
